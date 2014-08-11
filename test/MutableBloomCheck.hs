af13c1140675d2190de81a551b479f2cadc86ed4
Ray R <info@enlivn.io>
2014-08-10 22:34:59 -0400

    Adding more tests.

--- test/MutableBloomCheck.hs
+++ test/MutableBloomCheck.hs
@@ -6,6 +6,9 @@ import qualified MutableBloomFilter as M
 import System.Random (Random)
 import Test.QuickCheck
 import Control.Monad.ST (ST, runST)
+import Data.Word (Word32)
+import Data.ByteString.Lazy as L (ByteString, pack)
+import Data.ByteString as S (ByteString, pack)
 --import Types
 
 newtype FalsePositiveRate = FalsePositiveRate Double
@@ -14,6 +17,12 @@ newtype FalsePositiveRate = FalsePositiveRate Double
 instance Arbitrary FalsePositiveRate where
     arbitrary = choose (eps, 1-eps)
         where eps = 1e-6
+
+instance Arbitrary L.ByteString where
+    arbitrary = fmap L.pack arbitrary
+
+instance Arbitrary S.ByteString where
+    arbitrary = fmap S.pack arbitrary
 
 runNTests :: Testable a => Int -> a -> IO ()
 runNTests n = quickCheckWith (stdArgs{maxSuccess=n})
@@ -24,10 +33,22 @@ prop_elem_inserted_always_exists (FalsePositiveRate falsePositiveRate) element =
                                                                           flip M.elem element
 
 main :: IO ()
-main = runNTests 500 (prop_elem_inserted_always_exists :: FalsePositiveRate -> Int -> Bool)
+main = do
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> Char -> Bool)
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> Int -> Bool)
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> Float -> Bool)
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> Double -> Bool)
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> String -> Bool)
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> L.ByteString -> Bool)
+    runNTests 5000 (prop_elem_inserted_always_exists :: FalsePositiveRate -> S.ByteString -> Bool)
+
 --main = do
 --    let fp = 0.3080696608225276
---        mbps = runST $ mutBitsPerSlice `fmap` (M.new fp 1::ST s (MutableBloom s Int))
---    print (mbps)
+--        n  = 2100937750 :: Word32
+--        --res :: Bool
+--        --res = runST $ (M.new fp n::ST s (M.MutableBloom s Int)) >>= (flip M.elem) 20
+--        mbps = runST $ mutBitsPerSlice `fmap` (M.new fp n::ST s (M.MutableBloom s Int))
+ --   --print res
+ --   print mbps
 --    res <- return $ (prop_elem_inserted_exists :: FalsePositiveRate -> String -> Bool) (FalsePositiveRate fp) "a"
 --    print res
