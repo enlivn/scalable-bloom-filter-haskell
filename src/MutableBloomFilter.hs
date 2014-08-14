0258220da7ab8f929fa70ae0377ee62cfca6c514
Ray R <info@enlivn.io>
2014-08-14 19:20:17 -0400

    Preparing for scalable filters.

--- src/MutableBloomFilter.hs
+++ src/MutableBloomFilter.hs
@@ -36,7 +36,7 @@ import Types
 -- The first argument is the false positive rate desired (between 0 and 1) (P)
 -- The second argument is the capacity of the filter (n)
 new :: Hashable a => Double -> Word32 -> ST s (MutableBloom s a)
-new p n = uncurry new' (calculateFilterParams p n)
+new p n = uncurry (new' n) (calculateFilterParams p n)
 
 -- | Create a mutable bloom filter and initialize it with the list passed in.
 -- The first argument is the false positive rate desired (between 0 and 1) (P)
@@ -51,12 +51,13 @@ fromList p initList = new p (genericLength initList) >>= \mb -> do
 toImmutable :: (forall s. ST s (MutableBloom s a)) -> ImmutableBloom a
 toImmutable mb = runST $ do
     mutableBloom <- mb
-    let bitsPerSlice = mutBitsPerSlice mutableBloom
+    let capacity = mutCap mutableBloom
+        bitsPerSlice = mutBitsPerSlice mutableBloom
         hashFuns = mutHashFns mutableBloom
     f <- unsafeFreeze $ mutBitArray mutableBloom -- for copying STUArray -> UArray, unsafeFreeze is
                                                  -- O(n) if compiled without -o,
                                                  -- O(1) if compiled with -o
-    return $ ImmutableBloom bitsPerSlice hashFuns f
+    return $ ImmutableBloom capacity bitsPerSlice hashFuns f
 
 -- | Convert a mutable bloom filter to an immutable bloom filter in ST
 --toImmutable' :: Hashable a => (MutableBloom s a) -> ST s (ImmutableBloom a)
@@ -76,7 +77,7 @@ calculateFilterParams :: Double -> Word32 -> (Int, Word32)
 calculateFilterParams p n | n <= 0 = error "n must be strictly positive"
                           | p <= 0 || p >= 1 = error "p must be between 0 and 1"
                           | otherwise = (k, fromIntegral m)
-    where k = ceiling $ logBase 2 (1 / p) :: Int-- k = log2 (1/P)
+    where k = ceiling $ logBase 2 (1 / p) :: Int -- k = log2 (1/P)
           m = ceiling $ n' * abs (log p) / (k' * log 2 **2) -- m = n * abs(log(P))/(k*(ln2)^2)
           n' = fromIntegral n :: Double
           k' = fromIntegral k :: Double
@@ -84,13 +85,13 @@ calculateFilterParams p n | n <= 0 = error "n must be strictly positive"
 -- | Create a mutable bloom filter
 -- The first argument is the number of slices in the filter (k)
 -- The second argument is the number of bits in each slice (m)
-new' :: Hashable a => Int -> Word32 -> ST s (MutableBloom s a)
-new' numSlices bitsPerSlice = return . MutableBloom bitsPerSlice (genHashes numSlices) =<< newArray (0, _M) False
+new' :: Hashable a => Word32 -> Int -> Word32 -> ST s (MutableBloom s a)
+new' capacity numSlices bitsPerSlice = return . MutableBloom capacity bitsPerSlice (genHashes numSlices) =<< newArray (0, _M) False
     where _M = fromIntegral numSlices * bitsPerSlice -- ^ total number of bits in the filter (M = k * m)
 
 -- | Returns the total length (M = m*k) of the filter.
 length :: MutableBloom s a -> ST s Word32
-length (MutableBloom _ _ bitArray) = fmap ((1 +) . snd) (getBounds bitArray)
+length filt = fmap ((1 +) . snd) (getBounds . mutBitArray $ filt)
 
 -- | Inserts an element into the filter.
 -- The first argument is the filter
--- src/Types.hs
+++ src/Types.hs
@@ -9,12 +9,14 @@ import Data.Array.Unboxed (UArray)
 import Data.Word (Word32)
 
 data MutableBloom s a = MutableBloom {
+                            mutCap :: Word32, -- ^ capacity of filter
                             mutBitsPerSlice :: Word32, -- ^ number of bits per slice
                             mutHashFns :: a -> [Word32], -- ^ hash functions
                             mutBitArray :: STUArray s Word32 Bool -- ^ array storing the state of the filter
                         }
 
 data ImmutableBloom a = ImmutableBloom {
+                            immutCap :: Word32, -- ^ capacity of filter
                             immutBitsPerSlice :: Word32, -- ^ number of bits per slice
                             immutHashFns :: a -> [Word32], -- ^ hash functions
                             immutBitArray :: UArray Word32 Bool -- ^ array storing the state of the filter.
