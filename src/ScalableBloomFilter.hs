8a38fb96e705d1ee0b8a92a0a422f83aa41de387
Ray R <info@enlivn.io>
2014-08-16 15:38:56 -0400

    Adding elem and notElem functions to ScalableBloomFilter.

--- src/ScalableBloomFilter.hs
+++ src/ScalableBloomFilter.hs
@@ -10,8 +10,6 @@ the compounded error probability of the scalable filter converges to the value
 specified in the new function.
 -}
 
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module ScalableBloomFilter(new,
                            size,
                            insert,
@@ -19,11 +17,12 @@ module ScalableBloomFilter(new,
                            elem,
                            notElem) where
 
+import Control.Monad (liftM)
 import Control.Monad.ST (ST, runST)
 import Data.Array.MArray (getElems, newArray, writeArray)
 import Data.Word (Word32)
 import Hash.Hash
-import qualified MutableBloomFilter as M (new, size, isFull, insert)
+import qualified MutableBloomFilter as M (new, size, isFull, insert, elem)
 import Prelude hiding (elem, notElem)
 import Types
 
@@ -43,16 +42,16 @@ new p = do
 -- filters.
 size :: ScalableBloom s a -> ST s Word32
 size filt = do
-    filters <- getElems (filtArray filt)
-    fmap sum $ mapM M.size filters
+    constituentFilters <- getElems (filtArray filt)
+    fmap sum $ mapM M.size constituentFilters
 
 -- | Inserts an element into the filter.
 -- The first argument is the filter
 -- The second argument is the element to insert
 insert :: Hashable a => ScalableBloom s a -> a -> ST s ()
 insert filt element = do
-    filters <- getElems (filtArray filt)
-    let lastMb = last filters
+    constituentFilters <- getElems (filtArray filt)
+    let lastMb = last constituentFilters
         p = (mutErrRate lastMb) * r -- Pi = Pi-1 * r
         c = (mutCap lastMb) * s     -- Mi = Mi-1 * s
         r = 0.9
@@ -60,10 +59,10 @@ insert filt element = do
     lastFilterIsFull <- M.isFull lastMb
     if lastFilterIsFull then do
         -- create a new bloom filter, add the element to it and add the filter itself
-        -- to the array of bloom filters in the scalable filter
+        -- to the array of constituent filters in the scalable filter
         newFilter <- M.new p c
         M.insert newFilter element
-        writeArray (filtArray filt) (length filters) newFilter
+        writeArray (filtArray filt) (length constituentFilters) newFilter
     else M.insert lastMb element
 
 -- | Inserts multiple elements into the filter.
@@ -73,9 +72,19 @@ insertList :: Hashable a => ScalableBloom s a -> [a] -> ST s ()
 insertList filt = mapM_ (insert filt)
 
 -- | Returns True if the element is in the filter
+-- There is a small chance that True will be returned even if the element
+-- is NOT in the filter
 elem :: ScalableBloom s a -> a -> ST s Bool
-elem filt element = undefined
+elem filt element = do
+    constituentFilters <- liftM reverse $ getElems $ filtArray filt -- reverse is so we start looking in the filters
+                                                                    -- that were most recently added
+    find element constituentFilters
+    where find :: a -> [MutableBloom s a] -> ST s Bool
+          find _ [] = return False
+          find e (x:xs) = M.elem x e >>= \found ->
+            if found then return True
+            else find e xs
 
 -- | Complement of 'elem'.
 notElem :: ScalableBloom s a -> a -> ST s Bool
-notElem filt element = undefined
+notElem filt element = liftM not $ elem filt element
